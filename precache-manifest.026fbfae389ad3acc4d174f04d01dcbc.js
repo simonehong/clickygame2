6ae2347f4201df282e4a7d9e24c443d1931eb12a
Simon Hong <simon_hong1@yahoo.com>
2019-03-12 18:10:22 -0600

    Updates

---
+++ precache-manifest.026fbfae389ad3acc4d174f04d01dcbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "72ca1f2086e67b32f8ca",
+    "url": "/clickygame/static/js/main.72ca1f20.chunk.js"
+  },
+  {
+    "revision": "9a03d35e8bafc42cc333",
+    "url": "/clickygame/static/js/1.9a03d35e.chunk.js"
+  },
+  {
+    "revision": "72ca1f2086e67b32f8ca",
+    "url": "/clickygame/static/css/main.6d7fe353.chunk.css"
+  },
+  {
+    "revision": "02678ba9b3b17b18eed2d691b0d1fa6d",
+    "url": "/clickygame/index.html"
+  }
+];
